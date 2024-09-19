4c9c06da51c38299f0a5c37755dbf60cee7b57bb
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-09-19 09:22:40 +0530

    Project Template

---
+++ HttpRequest/Backend/Data/DataStore.cs
@@ -0,0 +1,56 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Data
+{
+    public static class DataStore
+    {
+        private static List<Employee> employees = new();
+
+
+        public static List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public static Employee GetEmployeeById(Guid id)
+        {
+            return employees.Where(x=>x.Id == id).FirstOrDefault();
+        }
+
+        public static Guid AddEmployee(Employee employee) 
+        {
+            employee.Id = Guid.NewGuid();
+            employees.Add(employee);
+            return employee.Id;
+        }
+
+        public static bool UpdateEmployee(Employee employee)
+        {
+            var existingEmployee = employees.Where(x => x.Id == employee.Id).FirstOrDefault();
+
+            if (existingEmployee != null)
+            {
+                existingEmployee.Name = employee.Name;
+                existingEmployee.Age = employee.Age;
+                existingEmployee.EmailAddress = employee.EmailAddress;
+                existingEmployee.PhoneNo = employee.PhoneNo;
+                return true;  
+            }
+
+            return false; 
+        }
+
+        public static bool DeleteEmployee(Guid id)
+        {
+            var employee = employees.Where(x => x.Id == id).FirstOrDefault();
+
+            if (employee != null)
+            {
+                employees.Remove(employee);
+                return true;  
+            }
+
+            return false;  
+        }
+    }
+}
