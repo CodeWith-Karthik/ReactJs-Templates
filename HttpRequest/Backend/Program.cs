e8d0219e8bfb474b581b6fd3c0dc4c55a44931b3
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-09-20 15:59:54 +0530

    Project Template

--- HttpRequest/Backend/Program.cs
+++ HttpRequest/Backend/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CustomPolicy",x=>
+    x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
 
 builder.Services.AddControllers();
 
@@ -20,14 +29,16 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CustomPolicy");
 
 app.UseHttpsRedirection();
 
